bce6cb561eff88c3315ad95215df13790bf5806b
TongLi <69977497+Tong-L1@users.noreply.github.com>
2023-12-08 00:34:42 +0800

    Update main.py
    Signed-off-by: TongLi <69977497+Tong-L1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import json
 import os
 
 
-# minecraft/models/item/xxx.json
 def isEmpty(file_path):
     if not os.path.exists(file_path):
         return True
 
 
 def minecraft(pack_name, minecraft_item, custom_item_name, custom_model_data):
-    file_path = 'minecraft/models/item/' + minecraft_item + '.json'
+    file_dir = 'minecraft/models/item/'
+    file_name = minecraft_item + '.json'
+    file_path = file_dir + file_name
+    if isEmpty(file_dir):
+        os.makedirs(file_dir)
     if isEmpty(file_path):
-        with open(file_path, 'w+') as f:
+        with open(file_path, 'w') as f:
             layer0 = {"layer0": "minecraft:item/" + minecraft_item}
             json_j = {"overrides": [], "parent": "minecraft:item/handheld", "textures": layer0}
             f.write(json.dumps(json_j, indent=4))
@@ -23,13 +26,15 @@ def minecraft(pack_name, minecraft_item, custom_item_name, custom_model_data):
             json_load["overrides"].append(overrides)
         with open(file_path, 'r+') as fw:
             fw.write(json.dumps(json_load, indent=4))
-    # write model
     pack(pack_name, custom_item_name)
 
 
-# xxx/models/item/xxx.json
 def pack(pack_name, custom_item_name):
-    file_path = pack_name + '/models/item/' + custom_item_name + '.json'
+    file_dir = pack_name + '/models/item/'
+    file_name = custom_item_name + '.json'
+    file_path = file_dir + file_name
+    if isEmpty(file_dir):
+        os.makedirs(file_dir)
     with open(file_path, 'w') as f:
         textures = {"layer0": pack_name + ":item/" + custom_item_name}
         json_j = {"parent": "minecraft:item/handheld", "textures": textures}
@@ -37,4 +42,4 @@ def pack(pack_name, custom_item_name):
 
 
 if __name__ == '__main__':
-    minecraft('myserver', 'paper', 'flag', 122002)
+    minecraft('examplename', 'paper', 'flag0', 122001)
